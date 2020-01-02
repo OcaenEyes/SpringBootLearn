20bfaee76fddb6d4a2ce4f08498ae4544da16a4c
OcaenEyes <1450136519@qq.com>
2020-01-02 19:40:09 +0800

    master

---
+++ reactiveweblearn/src/main/java/com/gzy/reactiveweblearn/ReactiveweblearnApplication.java
@@ -0,0 +1,13 @@
+package com.gzy.reactiveweblearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveweblearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveweblearnApplication.class, args);
+    }
+
+}
---
+++ reactiveweblearn/src/test/java/com/gzy/reactiveweblearn/ReactiveweblearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzy.reactiveweblearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveweblearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
