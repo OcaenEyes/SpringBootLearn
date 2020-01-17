2628ef4b3ce3c2a73610b4950db025b121e20109
OcaenEyes <1450136519@qq.com>
2020-01-17 23:10:06 +0800

    master

---
+++ jpademo/src/main/java/com/gzy/jpademo/config/DataSourceConfig.java
@@ -0,0 +1,44 @@
+package com.gzy.jpademo.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    // 配置第一数据库
+    @Bean(name = "primaryDataSource")
+    // 作为默认数据库使用
+    @Primary
+    @Qualifier("primaryDataSource")
+    @ConfigurationProperties("spring.datasource.primary")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    // 配置第二个数据库
+    @Bean(name = "secondaryDataSource")
+    @Qualifier("secondaryDataSource")
+    @ConfigurationProperties("spring.datasource.secondary")
+    public DataSource secondaryDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "primaryJdbcTemplate")
+    public JdbcTemplate primaryJdbcTemplate(@Qualifier("primaryDataSource") DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+
+    @Bean(name ="secondaryJdbcTemplate" )
+    public JdbcTemplate secondaryJdbcTemplate(@Qualifier("secondaryDataSource") DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+}
--- jpademo/src/test/java/com/gzy/jpademo/JpademoApplicationTests.java
+++ jpademo/src/test/java/com/gzy/jpademo/JpademoApplicationTests.java
@@ -5,7 +5,11 @@ import com.gzy.jpademo.entity.User;
 import com.gzy.jpademo.repository.RoomRepository;
 import com.gzy.jpademo.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.annotation.Resource;
 
@@ -33,5 +37,24 @@ class JpademoApplicationTests {
         String s = user.toString();
         System.out.println("输出userId 为1 的账户");
         System.out.println(s);
+    }
+
+    @Autowired
+    @Qualifier("primaryJdbcTemplate")
+    private JdbcTemplate jdbcTemplate1;
+
+    @Autowired
+    @Qualifier("secondaryJdbcTemplate")
+    private JdbcTemplate jdbcTemplate2;
+
+    @Test
+    public void dataSourceTest(){
+        System.out.println("开始查询数据");
+        SqlRowSet sqlRowSet = jdbcTemplate1.queryForRowSet("SELECT * FROM account ");
+        System.out.println(sqlRowSet);
+        SqlRowSet sqlRowSet1 = jdbcTemplate2.queryForRowSet("SELECT * FROM students");
+        System.out.println(sqlRowSet1);
+        System.out.println("查询数据结束");
+
     }
 }
